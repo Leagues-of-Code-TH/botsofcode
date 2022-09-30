a03f71478c789de1d1d7fbf651caf7a25aea5d1c
PathonScript <pathonscript@gmail.com>
2022-09-30 22:03:33 +0700

    feat: working member join welcome

--- src/events/memberjoin.ts
+++ src/events/memberjoin.ts
@@ -1,3 +1,4 @@
+import { EmbedBuilder, TextChannel } from "discord.js";
 import type { ArgsOf, Client } from "discordx";
 import { Discord, On } from "discordx";
 
@@ -5,6 +6,31 @@ import { Discord, On } from "discordx";
 export class MemberJoin {
   @On()
   guildMemberAdd([member]: ArgsOf<"guildMemberAdd">, client: Client): void {
-    console.log("A new member joined", client.user?.username, member.displayName);
+    const welcomeChannelId = "1025383372299710464";
+
+    if (welcomeChannelId !== null) {
+      const welcomeChannel = client.channels.cache.get(
+        welcomeChannelId
+      ) as TextChannel;
+
+      const welcomeMessageEmbed = new EmbedBuilder()
+        .setColor("Aqua")
+        .setTitle("New Member")
+        .setDescription(
+          `Welcome ${member.user.username} to the Leagues of Code \n` +
+            "Use `/rolemenu` to choose your pings and languages roles\n" +
+            "Enjoy your stay!"
+        );
+
+      welcomeChannel.send({
+        embeds: [welcomeMessageEmbed],
+      });
+    }
+
+    console.log(
+      "A new member joined",
+      client.user?.username,
+      member.displayName
+    );
   }
 }
