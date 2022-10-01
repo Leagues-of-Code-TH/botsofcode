06b1f572a59ec971cce1c5340b9a683dc6c9768b
PathonScript <pathonscript@gmail.com>
2022-10-01 14:16:58 +0700

    feat!: removed unused commands, share name across commands

--- src/commands/verify.ts
+++ src/commands/verify.ts
@@ -16,13 +16,31 @@ import { ButtonComponent, Discord, Slash, SlashOption } from "discordx";
 
 @Discord()
 export class Command {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
   @Slash({
     name: "verify",
     description: "Verify yourself and join Leagues of Code!",
   })
-  async verify(interaction: CommandInteraction): Promise<void> {
+  async verify(
+    @SlashOption({
+      description: "Enter your real name",
+      name: "name",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    })
+    name: string | undefined,
+    interaction: CommandInteraction
+  ): Promise<void> {
+    this.name = name!;
+
     await interaction.deferReply({ ephemeral: true });
 
+    // Class Buttons
     const PythonButton = new ButtonBuilder()
       .setLabel("Python")
       .setEmoji("<:python:1025584887337590834>")
@@ -35,23 +53,26 @@ export class Command {
       .setStyle(ButtonStyle.Primary)
       .setCustomId("cplus");
 
+    // Button rows
     const row =
       new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
         PythonButton,
         CplusButton
       );
 
-    interaction.editReply({
-      content: `${interaction.user.username}, Select your course!`,
+    // Selector
+    await interaction.editReply({
+      content: `${name}, Select your course!`,
       components: [row],
     });
   }
 
+  // Events after button press
   @ButtonComponent({ id: "python" })
   PythonButton(interaction: ButtonInteraction): void {
     interaction.reply({
       ephemeral: true,
-      content: `You've sent a verification request of **Python**<:python:1025584887337590834>! ${interaction.member}\nWait for an admin to approve your request.`,
+      content: `***${this.name}***, you've sent a verification request of **Python**<:python:1025584887337590834>! ${interaction.member}\nWait for an admin to approve your request.`,
     });
   }
 
@@ -59,7 +80,7 @@ export class Command {
   CplusButton(interaction: ButtonInteraction): void {
     interaction.reply({
       ephemeral: true,
-      content: `You've sent a verification request of **C++**<:cplus:1025584885034913802>! ${interaction.member}\nWait for an admin to approve your request.`,
+      content: `***${this.name}***, you've sent a verification request of **C++**<:cplus:1025584885034913802>! ${interaction.member}\nWait for an admin to approve your request.`,
     });
   }
 }
