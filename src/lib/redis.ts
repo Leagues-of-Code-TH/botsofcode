8118423fed370a994713cc0cdc9b26a65672b24e
Patato <pathonscript@gmail.com>
2022-10-11 09:53:24 +0700

    feat: implemented redis functions

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,4 +1,4 @@
-import { Client } from "redis-om";
+import { Client, Entity, Repository, Schema } from "redis-om";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -8,4 +8,35 @@ async function connect() {
   if (!client.isOpen()) {
     await client.open(process.env.REDIS_URL);
   }
+}
+
+class Student extends Entity {}
+let schema = new Schema(
+  Student,
+  {
+    discord: { type: "string" },
+    name: { type: "string" },
+    createdAt: { type: "date" },
+  },
+  {
+    dataStructure: "JSON",
+  }
+);
+
+type StudentData = {
+  discord: string;
+  name: string;
+  createdAt: Date;
+};
+
+export async function createStudent(data: StudentData): Promise<string> {
+  await connect();
+
+  const repository = client.fetchRepository(schema);
+
+  const student = repository.createEntity(data);
+
+  const id = await repository.save(student);
+
+  return id;
 }
