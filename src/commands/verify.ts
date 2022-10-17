e98ebc906d3dd9af7f3f1f0ff1ddc0766ccd097e
Patato <pathonscript@gmail.com>
2022-10-17 09:40:53 +0700

    increased command expire

--- src/commands/verify.ts
+++ src/commands/verify.ts
@@ -127,7 +127,7 @@ export class Command {
         embeds: [expiredEmbed],
       });
       console.log("Command Expired");
-    }, 1 * 60 * 1000);
+    }, 10 * 60 * 1000);
 
     if (guildId == process.env.SP_ID) {
       await interaction.editReply({
